6ef9d4552eeb5030f693b094b307d3b156507cf0
Anthony Aniobi <anthonyaniobi198@gmail.com>
2023-03-29 22:35:53 +0100

    Initial Commit

---
+++ Furniture App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Furniture App
+//
+//  Created by Anthony Aniobi on 29/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Furniture App/Furniture_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Furniture_AppApp.swift
+//  Furniture App
+//
+//  Created by Anthony Aniobi on 29/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Furniture_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
